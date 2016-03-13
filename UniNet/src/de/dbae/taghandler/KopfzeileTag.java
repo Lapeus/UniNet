7493cd6dc9892bc00951a5720f80f0fbf5809106
acker003 <acker003@uni-hildesheim.de>
2016-03-13 08:06:04 +0100

    Yeah :) TagLibraries funktionieren, Logout-Button leitet an Anmeldung.jsp weiter

---
+++ UniNet/src/de/dbae/taghandler/KopfzeileTag.java
@@ -0,0 +1,34 @@
+package de.dbae.taghandler;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.Writer;
+
+import javax.servlet.jsp.tagext.TagSupport;
+
+public class KopfzeileTag extends TagSupport {
+
+	private static final long serialVersionUID = -5650080610199722432L;
+	
+	public int doStartTag() {
+		Writer out = pageContext.getOut();
+		String kopfzeile = "";
+		FileReader fr;
+		try {
+			fr = new FileReader(pageContext.getServletContext().getRealPath("DesignHTML//Kopfzeile.txt"));
+			BufferedReader br = new BufferedReader(fr);
+			String zeile;
+			do {
+				zeile = br.readLine();
+				kopfzeile += zeile;
+			} while (zeile != null);
+			br.close();
+			out.append(kopfzeile);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return SKIP_BODY;
+	}
+
+}
---
+++ UniNet/src/de/dbae/taghandler/SeitenAufbauTag.java
@@ -0,0 +1,54 @@
+package de.dbae.taghandler;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.Writer;
+
+import javax.servlet.jsp.tagext.TagSupport;
+
+public class SeitenAufbauTag extends TagSupport {
+	
+	private static final long serialVersionUID = 929940257312046566L;
+
+	public int doStartTag() {
+		Writer out = pageContext.getOut();
+		String kopfzeile = "";
+		FileReader fr;
+		try {
+			fr = new FileReader(pageContext.getServletContext().getRealPath("DesignHTML//SeitenAufbau1.txt"));
+			BufferedReader br = new BufferedReader(fr);
+			String zeile = br.readLine();
+			while (zeile != null) {
+				kopfzeile += zeile;
+				zeile = br.readLine();
+			}
+			br.close();
+			out.append(kopfzeile);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return EVAL_BODY_INCLUDE;
+	}
+	
+	public int doEndTag() {
+		Writer out = pageContext.getOut();
+		String kopfzeile = "";
+		FileReader fr;
+		try {
+			fr = new FileReader(pageContext.getServletContext().getRealPath("DesignHTML//SeitenAufbau2.txt"));
+			BufferedReader br = new BufferedReader(fr);
+			String zeile = br.readLine();
+			while (zeile != null) {
+				kopfzeile += zeile;
+				zeile = br.readLine();
+			}
+			br.close();
+			out.append(kopfzeile);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return SKIP_BODY;
+	}
+}
